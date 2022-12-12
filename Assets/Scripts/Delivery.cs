2cb87a1d06b06701ab142a433c8ee6d09c9c4a14
idanbnbst <idanbnbst@gmail.com>
2022-12-12 09:11:50 +0200

    Game rework

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,122 +4,43 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    public bool hasPackage;
-    public int vehicleLevel, packageCollected, collisionOccured;
-    [SerializeField] int collisionAllowed = 3;
-    [SerializeField] float pickupDestroyTime = 0f;
-
-    [Header("Quest Colors")]
-    [SerializeField] Color32 hasQuestColor;
-    [SerializeField] Color32 noQuestColor;
     SpriteRenderer spriteRenderer;
     Sprite car1Sprite, car2Sprite, car3Sprite, bike1Sprite, bike2Sprite;
+    [SerializeField] bool inProgress;
+    [SerializeField] int totalDelivered = 0;
+    [SerializeField] int deliveredCount = 0;
+    [SerializeField] float pickupDestroyTime = 0f;
+    [Header("Delivery Indication")]
+    [SerializeField] Color32 defaultColor;
+    [SerializeField] Color32 inProgressColor;
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        car1Sprite = Resources.Load<Sprite>("Car 1");
-        car2Sprite = Resources.Load<Sprite>("Car 2");
-        car3Sprite = Resources.Load<Sprite>("Car 3");
-        bike1Sprite = Resources.Load<Sprite>("Motorcycle 1");
-        bike2Sprite = Resources.Load<Sprite>("Motorcycle 2");
-        vehicleLevel = packageCollected = collisionOccured = 0;
     }
-    public int getVehicleLevel()
-    {
-        return this.vehicleLevel;
-    }
-    public int getNumOfCollisions()
-    {
-        return this.collisionOccured;
-    }
-    void OnCollisionEnter2D(Collision2D other)
-    {
-        collisionOccured++;
-        if (vehicleLevel > 0 && collisionOccured == collisionAllowed) // Downgrade vehicle
-        {
-            switch (vehicleLevel)
-            {
-                case 1:
-                    spriteRenderer.sprite = car1Sprite;
-                    break;
-                case 2:
-                    spriteRenderer.sprite = car2Sprite;
-                    break;
-                case 3:
-                    spriteRenderer.sprite = car3Sprite;
-                    break;
-                case 4:
-                    spriteRenderer.sprite = bike1Sprite;
-                    break;
-            }
-            vehicleLevel--;
-            collisionOccured = 0;
-            Debug.Log("Vehicle has downgraded to level " + vehicleLevel);
-        }
-        else
-            Debug.Log("It's just a scratch! " + (collisionAllowed - collisionOccured) + " collisions remain");
-    }
-
+    public bool InProgress { get { return inProgress; } set { inProgress = value; } }
+    public int Delivered { get { return deliveredCount; } set { deliveredCount = value; } }
     void OnTriggerEnter2D(Collider2D other)
     {
         switch (other.tag)
         {
             case "Package":
-                if (!hasPackage)
+                if (!inProgress)
                 {
-                    hasPackage = true;
-                    packageCollected++;
-                    spriteRenderer.color = hasQuestColor;
+                    inProgress = true;
+                    spriteRenderer.color = inProgressColor;
                     Destroy(other.gameObject, pickupDestroyTime);
                     Debug.Log("Delivery picked");
                 }
                 break;
 
             case "Customer":
-                if (hasPackage)
+                if (inProgress)
                 {
-                    hasPackage = false;
-                    spriteRenderer.color = noQuestColor;
+                    inProgress = false;
+                    totalDelivered++;
+                    deliveredCount++;
+                    spriteRenderer.color = defaultColor;
                     Debug.Log("Package delivered to Customer");
-                    switch (vehicleLevel) // Upgrade vehicle
-                    {
-                        case 1:
-                            if (packageCollected >= 5)
-                            {
-                                vehicleLevel++;
-                                spriteRenderer.sprite = car3Sprite;
-                                collisionOccured = 0;
-                                Debug.Log("Vehicle has upgraded to level " + vehicleLevel);
-                            }
-                            break;
-                        case 2:
-                            if (packageCollected >= 10)
-                            {
-                                vehicleLevel++;
-                                spriteRenderer.sprite = bike1Sprite;
-                                collisionOccured = 0;
-                                Debug.Log("Vehicle has upgraded to level " + vehicleLevel);
-                            }
-                            break;
-                        case 3:
-                            if (packageCollected >= 13)
-                            {
-                                vehicleLevel++;
-                                spriteRenderer.sprite = bike2Sprite;
-                                collisionOccured = 0;
-                                Debug.Log("Vehicle has upgraded to level " + vehicleLevel);
-                            }
-                            break;
-                        default:
-                            if (packageCollected >= 2)
-                            {
-                                vehicleLevel++;
-                                spriteRenderer.sprite = car2Sprite;
-                                collisionOccured = 0;
-                                Debug.Log("Vehicle has upgraded to level " + vehicleLevel);
-                            }
-                            break;
-                    }
                 }
                 break;
         }
--- Assets/Scripts/DriverController.cs
+++ Assets/Scripts/DriverController.cs
@@ -1,41 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class DriverController : MonoBehaviour
 {
+    Delivery delivery;
+    SpriteRenderer spriteRenderer;
+    Sprite car1Sprite, car2Sprite, car3Sprite, bike1Sprite, bike2Sprite;
     float steerAmount, moveAmount, nitroAmount;
+    [SerializeField] int vehicleLevel = 1;
+    [SerializeField] int deliveryGoal = 2;
+    [Header("Collisions")]
+    [SerializeField] int collisionsAllowed = 3;
+    [SerializeField] int collisionOccured = 0;
+    [Header("Current Speeds")]
+    [SerializeField] int moveSpeed = 15;
+    [SerializeField] int steerSpeed = 125;
     [Header("Slow Speeds")]
     [SerializeField] int slowMoveSpeed = 10;
     [SerializeField] int slowSteerSpeed = 100;
-
-    [Header("Basic Speeds")]
-    [SerializeField] int moveSpeed = 15;
-    [SerializeField] int steerSpeed = 125;
-
     [Header("Boost Speeds")]
     [SerializeField] int boostMoveSpeed = 20;
     [SerializeField] int boostSteerSpeed = 150;
-
     [Header("Fast Speeds")]
     [SerializeField] int fastMoveSpeed = 22;
     [SerializeField] int fastSteerSpeed = 170;
-
     [Header("Super Speeds")]
     [SerializeField] int superMoveSpeed = 25;
     [SerializeField] int superSteerSpeed = 190;
-
     [Header("Bike Speeds")]
     [SerializeField] int bikeMoveSpeed = 30;
     [SerializeField] int bikeSteerSpeed = 200;
-
     [Header("Super Bike Speeds")]
     [SerializeField] int superBikeMoveSpeed = 35;
     [SerializeField] int superBikeSteerSpeed = 250;
-    Delivery deliveryVehicle;
     void Start()
     {
-        deliveryVehicle = FindObjectOfType<Delivery>();
+        delivery = FindObjectOfType<Delivery>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        LoadSpriteResources();
+    }
+    void LoadSpriteResources()
+    {
+        car1Sprite = Resources.Load<Sprite>("Car 1");
+        car2Sprite = Resources.Load<Sprite>("Car 2");
+        car3Sprite = Resources.Load<Sprite>("Car 3");
+        bike1Sprite = Resources.Load<Sprite>("Motorcycle 1");
+        bike2Sprite = Resources.Load<Sprite>("Motorcycle 2");
     }
     void Update()
     {
@@ -45,63 +53,128 @@ public class DriverController : MonoBehaviour
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount + nitroAmount, 0);
     }
+    public bool HasReachedMaxCollisions()
+    {
+        return collisionOccured == collisionsAllowed;
+    }
+    public int CollisionsRemainded()
+    {
+        return collisionsAllowed - collisionOccured;
+    }
+    void ModifyVehicle(int level, Sprite skin, int move, int steer, int newGoal, string message)
+    {
+        vehicleLevel += level;
+        collisionOccured = 0;
+        spriteRenderer.sprite = skin;
+        moveSpeed = move;
+        steerSpeed = steer;
+        deliveryGoal = newGoal;
+        if (!message.Equals(string.Empty))
+            Debug.Log(message);
+    }
+    void BoostVehicleSpeed()
+    {
+        switch (vehicleLevel)
+        {
+            case 2:
+                moveSpeed = fastMoveSpeed + 5; // 27
+                steerSpeed = fastSteerSpeed + 10; // 180
+                break;
+
+            case 3:
+                moveSpeed = superMoveSpeed + 5; // 30
+                steerSpeed = superSteerSpeed + 30; // 220
+                break;
+
+            case 4:
+                moveSpeed = bikeMoveSpeed + 3; // 33
+                steerSpeed = bikeSteerSpeed + 50; // 250
+                break;
+
+            case 5:
+                moveSpeed = superBikeMoveSpeed + 5; // 40
+                steerSpeed = superBikeSteerSpeed + 50; // 300
+                break;
+
+            default:
+                moveSpeed = boostMoveSpeed; // 20
+                steerSpeed = boostSteerSpeed; // 150
+                break;
+        }
+    }
+    void Downgrade()
+    {
+        switch (vehicleLevel)
+        {
+            case 2:
+                ModifyVehicle(-1, car1Sprite, slowMoveSpeed + 3, slowSteerSpeed + 15, 2,
+                "Vehicle has been downgraded to level " + vehicleLevel);
+                break;
+
+            case 3:
+                ModifyVehicle(-1, car2Sprite, slowMoveSpeed + 7, slowSteerSpeed + 30, 5,
+                    "Vehicle has been downgraded to level " + vehicleLevel);
+                break;
+
+            case 4:
+                ModifyVehicle(-1, car3Sprite, boostMoveSpeed, boostSteerSpeed, 10,
+                    "Vehicle has been downgraded to level " + vehicleLevel);
+                break;
+
+            case 5:
+                ModifyVehicle(-1, bike1Sprite, fastMoveSpeed, fastSteerSpeed, 18,
+                    "Vehicle has been downgraded to level " + vehicleLevel);
+                break;
+
+            default:
+                ModifyVehicle(0, car1Sprite, slowMoveSpeed, slowSteerSpeed, 2,
+                "Maximum collisions have been reached. Car is broken!");
+                break;
+        }
+    }
+    void Upgrade()
+    {
+        switch (vehicleLevel)
+        {
+            case 2:
+                ModifyVehicle(1, car3Sprite, superMoveSpeed, superSteerSpeed, 10,
+                "Vehicle has been upgraded to level " + vehicleLevel);
+                break;
+
+            case 3:
+                ModifyVehicle(1, bike1Sprite, bikeMoveSpeed, bikeSteerSpeed, 18,
+                "Vehicle has been upgraded to level " + vehicleLevel);
+                break;
+
+            case 4:
+                ModifyVehicle(1, bike2Sprite, superBikeMoveSpeed, superBikeSteerSpeed, 25,
+                "Vehicle has been upgraded to level " + vehicleLevel);
+                break;
+
+            default:
+                ModifyVehicle(1, car2Sprite, fastMoveSpeed, fastSteerSpeed, 5,
+                "Vehicle has been upgraded to level " + vehicleLevel);
+                break;
+        }
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
-        if (deliveryVehicle.getNumOfCollisions() > 2)
+        collisionOccured++;
+
+        if (HasReachedMaxCollisions())
         {
-            switch (deliveryVehicle.getVehicleLevel())
-            {
-                case 1:
-                    moveSpeed = slowMoveSpeed + 3; // 13
-                    steerSpeed = slowSteerSpeed + 15; // 115
-                    break;
-                case 2:
-                    moveSpeed = slowMoveSpeed + 7; // 17
-                    steerSpeed = slowSteerSpeed + 30; // 130
-                    break;
-                case 3:
-                    moveSpeed = boostMoveSpeed; // 20
-                    steerSpeed = boostSteerSpeed; // 150
-                    break;
-                case 4:
-                    moveSpeed = fastMoveSpeed; // 22
-                    steerSpeed = fastSteerSpeed; // 170
-                    break;
-                default:
-                    moveSpeed = slowMoveSpeed; // 10
-                    steerSpeed = slowSteerSpeed; // 100
-                    break;
-            }
+            Downgrade();
+            delivery.Delivered = 0;
         }
+        else
+            Debug.Log("It's just a scratch! " + CollisionsRemainded() + " collisions left.");
     }
     void OnTriggerEnter2D(Collider2D other)
     {
         switch (other.tag)
         {
             case "Booster":
-                switch (deliveryVehicle.getVehicleLevel())
-                {
-                    case 1:
-                        moveSpeed = fastMoveSpeed + 5; // 27
-                        steerSpeed = fastSteerSpeed + 10; // 180
-                        break;
-                    case 2:
-                        moveSpeed = superMoveSpeed + 5; // 30
-                        steerSpeed = superSteerSpeed + 30; // 220
-                        break;
-                    case 3:
-                        moveSpeed = bikeMoveSpeed + 3; // 33
-                        steerSpeed = bikeSteerSpeed + 50; // 250
-                        break;
-                    case 4:
-                        moveSpeed = superBikeMoveSpeed + 5; // 40
-                        steerSpeed = superBikeSteerSpeed + 50; // 300
-                        break;
-                    default:
-                        moveSpeed = boostMoveSpeed; // 20
-                        steerSpeed = boostSteerSpeed; // 150
-                        break;
-                }
+                BoostVehicleSpeed();
                 break;
         }
     }
@@ -110,29 +183,8 @@ public class DriverController : MonoBehaviour
         switch (other.tag)
         {
             case "Customer":
-                switch (deliveryVehicle.getVehicleLevel())
-                {
-                    case 1:
-                        moveSpeed = fastMoveSpeed;
-                        steerSpeed = fastSteerSpeed;
-                        break;
-                    case 2:
-                        moveSpeed = superMoveSpeed;
-                        steerSpeed = superSteerSpeed;
-                        break;
-                    case 3:
-                        moveSpeed = bikeMoveSpeed;
-                        steerSpeed = bikeSteerSpeed;
-                        break;
-                    case 4:
-                        moveSpeed = superBikeMoveSpeed;
-                        steerSpeed = superBikeSteerSpeed;
-                        break;
-                    default:
-                        moveSpeed = boostMoveSpeed - 5; // 15 -> Basic move speed
-                        steerSpeed = boostSteerSpeed - 25; // 125 -> Basic steer speed
-                        break;
-                }
+                if (delivery.Delivered >= deliveryGoal)
+                    Upgrade();
                 break;
         }
     }
